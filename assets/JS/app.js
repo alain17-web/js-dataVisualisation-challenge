0478b2b62ee68e8ae10f81a22094f80b274caf6e
Emerson Vandeputte <Emerson@Mac-mini-d-Emerson.local>
2021-07-22 22:09:49 +0200

    test rater

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,9 +1,19 @@
 
 
-  let data1 = document.getElementById('table1').textContent;
+  //let data1 = document.getElementById('table1').textContent;
 
-  console.log(data1);
+//  console.log(data1);
 
-  let data2 = document.getElementById('table2').textContent;
+ // let data2 = document.getElementById('table2').textContent;
 
-  console.log(data2);
+  //console.log(data2);
+
+
+
+ // const data = Element.querySelectorAll('td');
+
+ //console.log(data);
+
+ const select = document.querySelectorAll('td[display]');
+
+console.log(select); // 1, not 0!
