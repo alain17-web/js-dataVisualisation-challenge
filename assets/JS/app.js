2780d5f665a6dad4d46a321f106ed6c0ae62decf
alain17-web <web2020.alain@gmail.com>
2021-07-26 15:41:00 +0200

    charts ok

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,3 +1,7 @@
+let noScript = document.createElement('noscript');
+noScript.innerText = "Sorry, Javascript is not enabled on your browser";
+document.querySelector('body').insertAdjacentElement('beforeend',noScript);
+
 //dataChart (update every second)
 let dataChart = document.createElement('canvas');
 dataChart.setAttribute('id','chart');
@@ -18,38 +22,52 @@ const chartConfig = {
     options: {}
 };
 
-
+let chart = new Chart(
+    document.getElementById('chart'),
+    chartConfig
+);
 
 let xStart = 1;
 let yStart = 10;
 let length = 10;
 
-fetch(`https://canvasjs.com/services/data/datapoints.php?xstart=${xStart}&ystart=${yStart}&length=${length}&type=json`)
-.then( value => value.json())
-.then( data => {
-    console.log(data);
+function updateDataChart(){
+
+    fetch(`https://canvasjs.com/services/data/datapoints.php?xstart=${xStart}&ystart=${yStart}&length=${length}&type=json`)
+        .then( value => value.json())
+        .then( data => {
+        
     
-    data.forEach(value => {
-        chart.data.labels.push(value[0]);
-        chart.data.datasets[0].data.push(value[1])
-    })
+            data.forEach(value => {
+                chart.data.labels.push(value[0]);
+                chart.data.datasets[0].data.push(value[1]);
+                
+            })
+            
 
-    xStart = chart.data.labels.length + 1;
-    yStart = chart.data.datasets[0].data[chart.data.datasets[0].data.length - 1];
-    length = 1;
+            xStart = chart.data.labels.length + 1;
+            console.log(xStart);
+            yStart = chart.data.datasets[0].data[chart.data.datasets[0].data.length - 1];
+            console.log(yStart);
+            length = 1;
 
+            
+            
+            chart.update();
+            let time = setTimeout(() => updateDataChart(),1000);
+            button.addEventListener('click',function(){
+            clearTimeout(time);
+            })
+        })
+}
+let button = document.createElement('button');
+button.innerText = 'Click to freeze chart';
+button.setAttribute('id','button');
+document.getElementById('chart').insertAdjacentElement('afterend',button);
 
-
-})
-
-let chart = new Chart(
-    document.getElementById('chart'),
-    chartConfig
-);
-//faire une fonction update ?
-
-
-
+updateDataChart();
+        
+        
 
 //Chart table1
 let data1 = document.getElementById('table1');
@@ -73,25 +91,35 @@ const data = {
 };
 
 const config = {
-    type: 'line',
-    data,
-    options:{}
+    type: 'bar',
+    data: data,
+    options:{
+        scales:{
+            y:{
+                beginAtzero: true
+            }
+        }
+    },
 };
 
 data.labels = labels.map(value => value.innerText).filter(value => value !="");
 
 
+
 trElems.forEach(trElem => {
-    let tdElems = [...trElem.querySelectorAll('td')];
+    let tdElems =  [...trElem.querySelectorAll('td')];
+    
     let datas = tdElems.map(value => value.innerText === ":" ? 0 : value.innerText);
     
-    console.log(datas);
+    
 
     data.datasets.push({
         label: datas[0],
         data: datas.slice(1,datas.length).map(value => value.toString().replace(",",".")),
         backgroundColor: `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`,
-        borderColor: `rgb(${randomColor()},${randomColor()}, ${randomColor()})`
+        borderColor: `rgb(${randomColor()},${randomColor()}, ${randomColor()})`,
+        borderWidth: 1,
+        maxBarThickness : 5
         
     })
 })
@@ -106,8 +134,6 @@ let data2 = document.getElementById('table2');
 let chart2 = document.createElement('canvas');
 chart2.setAttribute("id","chartTable2");
 data2.insertAdjacentElement('beforebegin',chart2);
-
-
 
 
 labels = [...data2.querySelectorAll('thead tr th')];
@@ -153,34 +179,3 @@ let chartTable2 = new Chart(
 function randomColor(){
     return Math.floor(Math.random() * 256);}
 
-/*Bubble chart
-let chartBubble = document.createElement('canvas');
-chartBubble.setAttribute("id","chartBubble");
-chart2.insertAdjacentElement('afterend',chartBubble);
-
-const bubbleData = {
-    datasets:[{
-        label:'',
-        data:[]
-    }],
-
-};
-
-const bubbleConfig = {
-    type: 'bubble',
-    data: bubbleData,
-    options:{}
-};
-
-trElems.forEach(trElem => {
-    datas = [...trElem.querySelectorAll('td')].map(value => value.innerText);
-
-    bubbleData.datasets.data.push({
-        data: datas.slice(1,datas.length),
-        backgroundColor: 'rgb(255, 99, 132)'
-    })
-})  
-
-let bubble = new Chart(
-    document.getElementById('chartBubble'),
-    barConfig);*/
